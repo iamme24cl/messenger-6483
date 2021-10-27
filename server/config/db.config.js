7104bd1e560ad8bf2f869632b54522ddc21360c3
Chandra Lama <iamme24cl@gmail.com>
2021-10-27 15:22:51 -0700

    created a db.config file and added all configuration in db.js file to connect to database on local machine

---
+++ server/config/db.config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env
+
+module.exports = {
+  HOST: DB_HOST,
+  USER: DB_USERNAME,
+  PASSWORD: DB_PASSWORD,
+  DB: "messenger",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,26 @@
-const Sequelize = require("sequelize");
+const dbConfig = require("../config/db.config.js");
+const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+const db = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: 0,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
 });
+
+(async () => {
+  try {
+    await db.authenticate();
+    console.log('Connection has been successfully established')
+  } catch (error) {
+    console.log('Unable to connect to the database:', error);
+  }
+})();
+
 
 module.exports = db;
