b63f135c63a320b621db1a0c5ee024a9d10a6c95
Chandra Lama <iamme24cl@gmail.com>
2021-10-30 14:29:36 -0700

    changed messages back to be pushed into the list

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -14,7 +14,7 @@ export const addMessageToStore = (state, payload) => {
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
       const convoCopy = { ...convo };
-      convoCopy.messages.unshift(message);
+      convoCopy.messages.push(message);
       convoCopy.latestMessageText = message.text;
       return convoCopy;
     } else {
@@ -72,7 +72,7 @@ export const addNewConvoToStore = (state, recipientId, message) => {
     if (convo.otherUser.id === recipientId) {
       const newConvo = { ...convo };
       newConvo.id = message.conversationId;
-      newConvo.messages.unshift(message);
+      newConvo.messages.push(message);
       newConvo.latestMessageText = message.text;
       return newConvo;
     } else {
