9f38b818093f8d61d96e945a54b1019e010c1f53
Chandra Lama <iamme24cl@gmail.com>
2021-11-11 13:37:09 -0800

    implemented many to many assocaition for user and conversation

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,10 +4,15 @@ const Message = require("./message");
 
 // associations
 
-User.belongsTo(Conversation);
-Conversation.hasMany(User);
-Message.belongsTo(Conversation);
+User.belongsToMany(Conversation, { through: 'ConversationsUsers' });
+Conversation.belongsToMany(User, { through: 'ConversationsUsers' });
+
+User.hasMany(Message);
+Message.belongsTo(User);
 Conversation.hasMany(Message);
+Message.belongsTo(Conversation);
+
+
 
 module.exports = {
   User,
