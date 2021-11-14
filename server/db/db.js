757360ca0e2f655269c617d8771a9c77dd026c7b
Chandra Lama <iamme24cl@gmail.com>
2021-11-14 09:47:20 -0800

    changed db.js back to original

--- server/db/db.js
+++ server/db/db.js
@@ -1,24 +1,7 @@
 const Sequelize = require("sequelize");
 
-const dbConfig = require('../config/db.config.js');
-
-// const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-//   logging: false
-// });
-
-const db = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: 0
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+  logging: false
 });
-
-(async () => {
-  try {
-    await db.authenticate();
-    console.log('Connection has been successfully established')
-  } catch (error) {
-    console.log('Unable to connect to the database:', error);
-  }
-})();
 
 module.exports = db;
